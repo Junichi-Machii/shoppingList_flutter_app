4e3357441f05e549608286bd762031ce34f8ab23
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-08-15 11:31:19 +0900

    入力したアイテムをスワイプで消す

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -28,22 +28,37 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(child: Text("アイテムを追加してください。"),);
+    Widget content = const Center(
+      child: Text("アイテムを追加してください。"),
+    );
 
-    if (_groceryItems.isNotEmpty){
+    if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
-          itemCount: _groceryItems.length,
-          itemBuilder: (ctx, index) => ListTile(
-                title: Text(_groceryItems[index].name),
-                leading: Container(
-                  width: 24,
-                  height: 24,
-                  color: _groceryItems[index].category.color,
-                ),
-                trailing: Text(_groceryItems[index].quantity.toString()),
-              ));
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
+          ),
+        ),
+      );
     }
 
     return Scaffold(
@@ -56,7 +71,7 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body:content ,
+      body: content,
     );
   }
 }

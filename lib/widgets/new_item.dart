e7cd97aaa6688aad665c018bd4a04ab9aa4042ab
Junichi-Machii <junichi878338@hotmail.co.jp>
2023-08-16 12:16:15 +0900

    #16 Realtime database リクエストの処理と応答の待機

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list_app01/data/categories.dart';
 import 'package:shopping_list_app01/models/category.dart';
-import 'package:shopping_list_app01/models/grocery_item.dart';
+// import 'package:shopping_list_app01/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,13 +22,13 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
           'flutter-list-database-373f2-default-rtdb.firebaseio.com',
           'shopping-list.json');
-      http.post(
+      final response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -41,9 +41,15 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
-      // Navigator.of(context).pop(
-      //  
-      // );
+
+      print(response.body);
+      print(response.statusCode);
+     if(!context.mounted){
+      return;
+     }
+      Navigator.of(context).pop(
+       
+      );
     }
   }
 
